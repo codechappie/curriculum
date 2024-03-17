197959b432e3c9d3b0bbd9d4dfe2e8d936ef50d4
CodeChappie <acanalesa14@gmail.com>
2024-03-17 01:37:36 -0500

    feat: web updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
+      <head/>
       <body id="body" className={`dark ${inter.className}`}>{children}</body>
     </html>
   );
